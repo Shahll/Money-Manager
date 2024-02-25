1f127c4728d502788ab0b133bd5fb7dde82fc530
Shahll <130846011+Shahll@users.noreply.github.com>
2024-02-25 13:58:12 +0100

    refactor: added record requests

---
+++ src/main/java/com/shahll/money/manager/model/Requests/ExpenseRequest.java
@@ -0,0 +1,2 @@
+package com.shahll.money.manager.model.Requests;public class ExpenseRequest {
+}
---
+++ src/main/java/com/shahll/money/manager/model/Requests/TagNameRequest.java
@@ -0,0 +1,4 @@
+package com.shahll.money.manager.model.Requests;
+
+public record TagNameRecord(String oldName, String newName) {
+}
